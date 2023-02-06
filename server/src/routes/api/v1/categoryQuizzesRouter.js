fedf52ceb68eef53ee9839ee9644b71833848bba
camziny <cameronziny@gmail.com>
2023-02-06 18:49:44 -0500

    -fix quiz form and post method -style quiz form

--- server/src/routes/api/v1/categoryQuizzesRouter.js
+++ server/src/routes/api/v1/categoryQuizzesRouter.js
@@ -7,21 +7,23 @@ import uploadImage from "../../../services/uploadImage.js";
 
 const categoryQuizzesRouter = new express.Router({ mergeParams: true });
 
-categoryQuizzesRouter.post("/", uploadImage.single("image"), async (req, res) => {
+categoryQuizzesRouter.post("/", uploadImage.single("content"), async (req, res) => {
   const { answer } = cleanUserInput(req.body);
-  const userId = req.user.userId;
+  const { categoryId } = req.params;
+  const userId = req.user.id;
 
   try {
     const newQuiz = await Quiz.query().insertAndFetch({
       content: req.file.location,
       answer,
-      categoryId: req.params.id,
+      categoryId,
       userId,
     });
     await newQuiz.$relatedQuery("votes").insert({ userId, quizId: newQuiz.id, score: 1 });
     const serializedQuiz = await QuizSerializer.getSummary(newQuiz);
     return res.status(201).json({ quiz: serializedQuiz });
   } catch (error) {
+    console.log(error);
     if (error instanceof ValidationError) {
       return res.status(422).json({ errors: error });
     } else {
